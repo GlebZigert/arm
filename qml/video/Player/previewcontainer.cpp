4e5ee24b1bd86e0a01c52a450372cef11967c591
frfff <ff>
2023-04-29 13:36:32 +0300

    29 4

--- qml/video/Player/previewcontainer.cpp
+++ qml/video/Player/previewcontainer.cpp
@@ -7,14 +7,15 @@ PreviewContainer::PreviewContainer()
 
 QSharedPointer<PreviewMaker> PreviewContainer::get_previewmaker(QString url)
 {
-qDebug()<<"PreviewContainer::get_previewmaker(QString url) "<<url;
+    qDebug()<<"PreviewContainer::get_previewmaker(QString url) "<<url;
     if(map.contains(url)){
+        qDebug()<<"PreviewContainer::get_previewmaker [0]";
         return map.value(url);
     }
 
     QSharedPointer<PreviewMaker> maker = QSharedPointer<PreviewMaker>::create(url);
     map.insert(url,maker);
-
+    qDebug()<<"PreviewContainer::get_previewmaker [1]";
     return maker;
 
 }
